36855571dd05d7a0498c6e20704489574d7d896e
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2024-11-27 02:15:20 +0300

    react static page

---
+++ public/index2.js
@@ -0,0 +1,4 @@
+const h1 = document.createElement("h1")
+h1.textContent = "this is from the h1"
+
+document.getElementById("container").appendChild(h1)
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import logo from '../logo512.png'
+
+export default function Navbar(){
+    return (
+        <>
+            <header>
+                <nav>
+                    <img src={logo} alt="" />
+                    <h1>ReactFacts</h1>
+                </nav>
+            </header>
+        </>
+    )
+}
